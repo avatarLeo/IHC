b6ea13192c2a0b1843b9737117166e13aea18fe4
avatarLeo <leandroalmeida055@gmail.com>
2024-08-09 11:32:58 -0300

    Terminado o design da jaela de login

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton, QSplitter
+from PyQt5.QtWidgets import (QWidget, QApplication, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton,
+                            QSplitter, QSizePolicy)
 from PyQt5.QtGui import QPixmap
 import sys
 
@@ -11,10 +12,23 @@ class Janela(QWidget):
         self.create_component()
 
     def create_component(self):
+
+        with open('style.css', 'r') as file:
+            self.setStyleSheet(file.read())
+
+
         right_widget = QWidget()
         left_widget = QWidget()
+        left_container = QWidget()
+
+        left_widget.setProperty('class', 'left_widget')
         
         left_layout = QVBoxLayout()
+        container_layout = QVBoxLayout()
+        container_layout.addWidget(left_widget)
+        container_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        left_container.setLayout(container_layout)
+        left_container.setProperty('class', 'left_container')
         right_layout = QVBoxLayout()
         layout = QVBoxLayout()
 
@@ -43,34 +57,43 @@ class Janela(QWidget):
         input_password_layout.addWidget(self.icon_sound_password)
 
         self.btn_login = QPushButton('Login')
+        # self.btn_login.setSizePolicy(QSizePolicy())
+    
 
-        self.forgot_password = QLabel('Esquci a senha')
+        self.forgot_password = QPushButton('Esquci a senha')
+        self.forgot_password.setProperty('class', 'forgot_password')
 
         self.icon_profile = QLabel()
+        self.icon_profile.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.icon_profile.setPixmap(self.add_image(200, 200, 'img/profile.svg'))
 
-        left_layout.addWidget(self.icon_profile)
-        left_layout.addLayout(input_email_layout)
-        left_layout.addLayout(input_password_layout)
-        left_layout.addWidget(self.btn_login)
-        left_layout.addWidget(self.forgot_password)
+        left_layout.addWidget(self.icon_profile,stretch=3)
+        left_layout.addLayout(input_email_layout, 0)
+        left_layout.addLayout(input_password_layout, 1)
+        left_layout.addWidget(self.btn_login, 1)
+        left_layout.addWidget(self.forgot_password, 0, Qt.AlignmentFlag.AlignHCenter)
         left_widget.setLayout(left_layout)
+
+
+        left_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
         #right layout
 
         self.cat_icon = QLabel('Cat icon')
+        self.cat_icon.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.cat_icon.setPixmap(self.add_image(300, 300, 'img/cat.png'))
 
         self.create_profile = QPushButton('Criar usuário')
-        right_layout.addWidget(self.cat_icon)
-        right_layout.addWidget(self.create_profile)
+        self.create_profile.setProperty('class', 'create_profile')
+        right_layout.addWidget(self.cat_icon, 2)
+        right_layout.addWidget(self.create_profile, 0, Qt.AlignmentFlag.AlignCenter)
 
         right_widget.setLayout(right_layout)
 
 
         splitter_layout = QSplitter()
         
-        splitter_layout.addWidget(left_widget)
+        splitter_layout.addWidget(left_container)
         splitter_layout.addWidget(right_widget)
 
         splitter_layout.setSizes([600, 400])

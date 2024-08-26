04dc81ced2435747972a4f19c8ccf4e653fca0c3
avatarLeo <leandroalmeida055@gmail.com>
2024-08-25 21:16:06 -0300

    Adicionados os audios e gifs

--- cadastro.py
+++ cadastro.py
@@ -1,7 +1,7 @@
-from PyQt5.QtCore import Qt, QEvent
+from PyQt5.QtCore import Qt, QEvent, QSize
 from PyQt5.QtWidgets import (QWidget, QApplication, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton,
                             QSplitter, QRadioButton, QComboBox)
-from PyQt5.QtGui import QPixmap, QMovie
+from PyQt5.QtGui import QPixmap, QMovie, QIcon
 from acessibility import *
 import sys
 
@@ -23,16 +23,21 @@ class Janela_cadastro(QWidget):
         left_container = QWidget()
 
         left_widget.setProperty('class', 'left_widget')
+
+        btn_settings = QPushButton()
+        btn_settings.setIcon(QIcon(self.add_image(300, 300, 'img/settings.png')))
+        btn_settings.setIconSize(QSize(40, 40))
+        btn_settings.setProperty('class', 'settings')
         
         self.left_layout = QVBoxLayout()
         container_layout = QVBoxLayout()
+        container_layout.addWidget(btn_settings, 0, Qt.AlignmentFlag.AlignRight)
         container_layout.addWidget(left_widget)
         container_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         left_container.setLayout(container_layout)
         left_container.setProperty('class', 'left_container')
         right_layout = QVBoxLayout()
         layout = QVBoxLayout()
-
         
 
         input_nome_layout = QHBoxLayout()
@@ -74,10 +79,15 @@ class Janela_cadastro(QWidget):
         input_idade_layout.addWidget(self.txt_idade)
         input_idade_layout.addWidget(self.icon_sound_idade)
 
+
         self.btn_cadastrar = QPushButton('Cadastrar')
+        self.btn_cadastrar.installEventFilter(self)
+        self.btn_cadastrar.setDefault(True)
     
 
         self.btn_clear = QPushButton('Limpar')
+        self.btn_clear.installEventFilter(self)
+        self.btn_clear.setDefault(True)
         self.btn_clear.setProperty('class', 'limpar')
 
         self.title = QLabel('Cadastre o seu pet')
@@ -119,9 +129,9 @@ class Janela_cadastro(QWidget):
         self.setLayout(layout)
 
 
-    def add_image(self, width, height, path_image):
+    def add_image(self, widget, height, path_image):
         self.pixmap_profile = QPixmap(path_image)
-        pixmap_profile_resize = self.pixmap_profile.scaled(width, height, Qt.AspectRatioMode.IgnoreAspectRatio)
+        pixmap_profile_resize = self.pixmap_profile.scaled(widget, height, Qt.AspectRatioMode.IgnoreAspectRatio)
         return pixmap_profile_resize
 
 
@@ -130,6 +140,7 @@ class Janela_cadastro(QWidget):
         self.service_layout = QHBoxLayout()
         lbl_service = QLabel('Serviços:')
         self.service_comboBox = QComboBox()
+        self.service_comboBox.installEventFilter(self)
         service_list = ['Tosa e Banho', 'Veterinário', 'Banho']
 
         self.service_comboBox.addItems(service_list)
@@ -147,16 +158,39 @@ class Janela_cadastro(QWidget):
 
     def show_data(self):
         print(self.service_comboBox.currentIndex())
+        service_index = self.service_comboBox.currentIndex()
+
+        match service_index:
+            case 0:
+                self.service_type = 'Tosa e Banhho'
+                if self.deficiency_type() == COM_DEFICIENCIA_VISUAL:
+                    sounds('sound/cadastro/tosa-banho.mp3')
+                elif self.deficiency_type() == COM_DEFICIENCIA_AUDITIVA:
+                    self.movies('gifs/gifs-cadastro/tosa-banho.gif')
+            case 1:
+                self.service_type = 'Veterinário'
+                if self.deficiency_type() == COM_DEFICIENCIA_VISUAL:
+                    sounds('sound/cadastro/veterinario.mp3')
+                elif self.deficiency_type() == COM_DEFICIENCIA_AUDITIVA:
+                    self.movies('gifs/gifs-cadastro/veterinario.gif')
+            case 2:
+                self.service_type = 'Banho'
+                if self.deficiency_type() == COM_DEFICIENCIA_VISUAL:
+                    sounds('sound/cadastro/banho.mp3')
+                elif self.deficiency_type() == COM_DEFICIENCIA_AUDITIVA:
+                    self.movies('gifs/gifs-cadastro/banho.gif')
 
     def create_radio_button(self):
-        radio_cat = QRadioButton('Gato', self)
-        radio_cat.toggled.connect(self.update)
-        radio_dog = QRadioButton('Cachorro', self)
-        radio_dog.toggled.connect(self.update)
+        self.radio_cat = QRadioButton('Gato', self)
+        self.radio_cat.installEventFilter(self)
+        self.radio_cat.toggled.connect(self.update)
+        self.radio_dog = QRadioButton('Cachorro', self)
+        self.radio_dog.installEventFilter(self)
+        self.radio_dog.toggled.connect(self.update)
 
         radio_layout = QHBoxLayout()
-        radio_layout.addWidget(radio_cat)
-        radio_layout.addWidget(radio_dog)
+        radio_layout.addWidget(self.radio_cat)
+        radio_layout.addWidget(self.radio_dog)
 
         self.left_layout.addLayout(radio_layout, 1)
 
@@ -167,38 +201,56 @@ class Janela_cadastro(QWidget):
             print(rb.text())
 
 
-    def eventFilter(self, width, event):
+    def eventFilter(self, widget, event):
         if event.type() == QEvent.Type.FocusIn:
-            if width == self.txt_nome:
+            if widget == self.txt_nome:
                 if self.deficiency_type() == COM_DEFICIENCIA_VISUAL:
                     sounds('sound/cadastro/nome.mp3')
                 elif self.deficiency_type() == COM_DEFICIENCIA_AUDITIVA:
                     self.movies('gifs/gifs-cadastro/nome.gif')
-            elif width == self.txt_raca:
+            elif widget == self.txt_raca:
                 if self.deficiency_type() == COM_DEFICIENCIA_VISUAL:
                     sounds('sound/cadastro/raca.mp3')
                 elif self.deficiency_type() == COM_DEFICIENCIA_AUDITIVA:
                     self.movies('gifs/gifs-cadastro/raca.gif')
-            elif width == self.txt_idade:
+            elif widget == self.txt_idade:
                 if self.deficiency_type() == COM_DEFICIENCIA_VISUAL:
                     sounds('sound/cadastro/idade.mp3')
                 elif self.deficiency_type() == COM_DEFICIENCIA_AUDITIVA:
                     self.movies('gifs/gifs-cadastro/idade.gif')
-            elif width == self.btn_cadastrar:
+            elif widget == self.service_comboBox:
                 if self.deficiency_type() == COM_DEFICIENCIA_VISUAL:
-                    sounds('sound/login/criar-usuario.mp3')
+                    sounds('sound/cadastro/serviços.mp3')
                 elif self.deficiency_type() == COM_DEFICIENCIA_AUDITIVA:
-                    self.movies('gifs/gifs-login/criar-usuario.gif')
-            elif width == self.btn_clear:
+                    self.movies('gifs/gifs-cadastro/servicos.gif')
+            elif widget == self.radio_cat:
                 if self.deficiency_type() == COM_DEFICIENCIA_VISUAL:
-                    sounds('sound/login/esqueceu-senha.mp3')
+                    sounds('sound/cadastro/gato.mp3')
                 elif self.deficiency_type() == COM_DEFICIENCIA_AUDITIVA:
-                    self.movies('gifs/gifs-login/esqueceu-senha.gif')
+                    self.movies('gifs/gifs-cadastro/gato.gif')
+            elif widget == self.radio_dog:
+                if self.deficiency_type() == COM_DEFICIENCIA_VISUAL:
+                    sounds('sound/cadastro/cachorro.mp3')
+                elif self.deficiency_type() == COM_DEFICIENCIA_AUDITIVA:
+                    self.movies('gifs/gifs-cadastro/cachorro.gif')
+            elif widget == self.btn_cadastrar:
+                if self.deficiency_type() == COM_DEFICIENCIA_VISUAL:
+                    sounds('sound/cadastro/cadastrar.mp3')
+                elif self.deficiency_type() == COM_DEFICIENCIA_AUDITIVA:
+                    self.movies('gifs/gifs-cadastro/cadastrar.gif')
+            elif widget == self.btn_clear:
+                if self.deficiency_type() == COM_DEFICIENCIA_VISUAL:
+                    sounds('sound/cadastro/limpar.mp3')
+                elif self.deficiency_type() == COM_DEFICIENCIA_AUDITIVA:
+                    self.movies('gifs/gifs-cadastro/limpar.gif')
         elif QEvent.Type.KeyPress == event.type():
-            print(QEvent.Type.Enter, event.type())
+            print(Qt.Key.Key_Enter, QEvent.Type.KeyPress, event.key())
+            if Qt.Key_Return == event.key() and self.radio_cat == widget:
+                self.radio_cat.setChecked(True)
+            elif Qt.Key_Return == event.key() and self.radio_dog == widget:
+                self.radio_dog.setChecked(True)
 
-
-        return super().eventFilter(width, event)
+        return super().eventFilter(widget, event)
         
     
     def deficiency_type(self):
